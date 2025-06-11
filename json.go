acf9e25edfb5e5aaddfe17ef8ba2f20bac77c6b5
Spock1701B <160493005+Spock1701B@users.noreply.github.com>
2025-06-11 14:40:40 +0200

    resovled formatting issues

--- json.go
+++ json.go
@@ -30,10 +30,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	_,err = w.Write(dat)
+	_, err = w.Write(dat)
 	if err != nil {
 		log.Printf("error writing to header %s", err)
 		w.WriteHeader(500)
-		return 
+		return
 	}
 }
